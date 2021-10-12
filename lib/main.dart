2b9cada4f77c831ea91f8e3f3eed43d4f27824fa
Pascal Groß <pascal.gross@student.fh-kiel.de>
2021-10-12 15:39:53 +0200

    main.dart: added first and very basic iteration of a form

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,78 @@ void main() => runApp( Coindart() );
 
 class Coindart extends StatelessWidget {
 
+  const Coindart( {Key? key} ) : super( key: key);
+
   @override
   Widget build( BuildContext context ) {
+
+    const appTitle = "Coindart - Your Next Bull's Eye Trade";
+
     return MaterialApp(
-      title: "Coindart",
+      title: appTitle,
+
       home: Scaffold(
+        backgroundColor: Colors.white12,
+
         appBar: AppBar(
-          title: const Text( "Coindart - Your Next Bull's Eye Trade" )
+          backgroundColor: Colors.deepPurple,
+          title: const Text( appTitle )
         ),
-        body: const Center(
-          child: Text( "Welcome to Coindart" ),
+
+        body: const Login(),
         ),
+      );
+  }
+}
+
+/// form reference: https://flutter.dev/docs/cookbook/forms/validation
+class Login extends StatefulWidget {
+
+  const Login( {Key? key} ) : super( key: key );
+
+  @override
+  LoginState createState() {
+
+    return LoginState();
+  }
+
+}
+
+class LoginState extends State<Login> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build( BuildContext context ) {
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            validator: (value) {
+              if( value == null || value.isEmpty) {
+                return "Bitte gib einen Text ein.";
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric( vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if( _formKey.currentState!.validate() ) {
+                  ScaffoldMessenger.of( context ).showSnackBar(
+                    const SnackBar( content: Text( "Eingabe wird verarbeitet") ),
+                  );
+                }
+              },
+              child: const Text("Login"),
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
