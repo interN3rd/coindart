2dbcdb31bb83f408fe6e1b34109b9a69b44021b7
SirOktarius <alexander.neumann@student.fh-kiel.de>
2021-10-15 20:38:39 +0200

    Restructured main method, applied some global theme colors etc., connected and edited login route, small temporary bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: "Coindart",
-      /// routing reference: https://flutter.dev/docs/cookbook/navigation/named-routes
-      initialRoute: "/",
-      routes: {
-        "/": ( context ) => const Coindart(),
-        "/contact": ( context ) => const Contact(),
-        "/imprint": ( context ) => const Imprint(),
-      },
-    ),
-  );
-}
+void main() => runApp(const Coindart());
 
 class Coindart extends StatelessWidget {
 
@@ -33,111 +20,127 @@ class Coindart extends StatelessWidget {
 
     return MaterialApp(
 
-      title: appTitle,
+        title: "Coindart",
+        /// routing reference: https://flutter.dev/docs/cookbook/navigation/named-routes
+        routes: {
+          "/contact": ( context ) => const Contact(),
+          "/imprint": ( context ) => const Imprint(),
+          "/login": ( context ) => const Login(),
+          "/register": ( context ) => const Register(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          scaffoldBackgroundColor: Colors.white12,
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.deepPurple,
+              foregroundColor: Colors.white,
+              centerTitle: true
+          ),
+        ),
 
-      home: Scaffold(
-
-        backgroundColor: Colors.white12,
+      home: Builder(
+        builder: (context) => Scaffold(
 
         appBar: AppBar(
 
-          backgroundColor: Colors.deepPurple,
           title: const Text( appTitle )
 
         ),
 
         body: Container(
 
-            padding: const EdgeInsets.all( 30 ),
+          padding: const EdgeInsets.all( 30 ),
 
-          child: Expanded(
+          child: Column(
 
-            child: Column(
+              children:  <Widget> [
 
-                children:  <Widget> [
-
-                  const Text(
-                      welcomeMessage,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17
-                  ) ),
-                  const Divider(),
-                  const Text(
-                    loginPrompt,
+                const Text(
+                    welcomeMessage,
                     textAlign: TextAlign.justify,
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17
-                    ) ),
-                  const Divider(),
+                        color: Colors.white,
+                        fontSize: 17
+                ) ),
+                const Divider(),
+                const Text(
+                  loginPrompt,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17
+                  ) ),
+                const Divider(),
 
-                  Row(
+                Row(
 
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: <Widget> [
+
+                    ElevatedButton(
+
+                      child: const Text( "Login" ),
+                      onPressed:  () {
+                        Navigator.pushNamed( context, "/login" );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      )
+                    ),
+
+                    const SizedBox( width: 50),
+
+                    ElevatedButton(
+                      child: const Text( "Register" ),
+                      onPressed:  () {
+                        Navigator.pushNamed( context, "/register" );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>( Colors.deepPurple ),
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      )
+                    )
+
+                  ]
+                ),
+
+                const Spacer(),
+                Row(
+
+                    mainAxisAlignment: MainAxisAlignment.start,
 
                     children: <Widget> [
 
-                      ElevatedButton(
+                      TextButton(
 
-                        child: const Text( "Login" ),
-                        onPressed: null,
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
-                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        )
-                      ),
+                        onPressed: () {
+                          Navigator.pushNamed( context, "/contact" );
+                        },
+                        child: const Text(
+                            "Contact",
+                            style: TextStyle(
+
+                                color: Colors.deepPurple
+                        ))
+                    ),
 
                       const SizedBox( width: 50),
 
-                      ElevatedButton(
-                        child: const Text( "Register" ),
-                        onPressed: null,
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>( Colors.deepPurple ),
-                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        )
-                      )
-
-                    ]
-                  ),
-
-                  const Spacer(),
-                  Row(
-
-                      mainAxisAlignment: MainAxisAlignment.start,
-
-                      children: <Widget> [
-
-                        TextButton(
-
-                          onPressed: () {
-                            Navigator.pushNamed( context, "/contact" );
-                          },
-                          child: const Text(
-                              "Contact",
-                              style: TextStyle(
-
-                                  color: Colors.deepPurple
-                          ))
-                      ),
-
-                        const SizedBox( width: 50),
-
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed( context, "/imprint" );
-                        },
-                        child: const Text(
-                            "Imprint",
-                            style: TextStyle(
-                                color: Colors.deepPurple
-                        ))
-                      )
-                    ]
-                  )
-                ]
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed( context, "/imprint" );
+                      },
+                      child: const Text(
+                          "Imprint",
+                          style: TextStyle(
+                              color: Colors.deepPurple
+                      ))
+                    )
+                  ]
+                )
+              ]
       ) ) ) ) );
   }
 }
@@ -180,19 +183,14 @@ class Imprint extends StatelessWidget {
 
     return Scaffold(
 
-      backgroundColor: Colors.white12,
-
         appBar: AppBar(
 
-            backgroundColor: Colors.deepPurple,
             title: const Text( "Imprint"),
         ),
 
       body: Container(
 
         padding: const EdgeInsets.all( 30 ),
-
-        child: Expanded(
 
           child: Column(
 
@@ -222,60 +220,121 @@ class Imprint extends StatelessWidget {
               ),
             ],
           ),
-        ),
       ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+
+  const Login( { Key? key } ) : super( key: key );
+
+  @override
+  Widget build( BuildContext context ) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+
+        title: const Text("Login"),
+      ),
+
+      body: const Center(
+        child: LoginForm(),
+      ) ,
     );
   }
 }
 
 /// form reference: https://flutter.dev/docs/cookbook/forms/validation
-class Login extends StatefulWidget {
+class LoginForm extends StatefulWidget {
 
-  const Login( {Key? key} ) : super( key: key );
+  const LoginForm( {Key? key} ) : super( key: key );
 
   @override
-  LoginState createState() {
-
-    return LoginState();
+  _LoginState createState() {
+    return _LoginState();
   }
 
 }
 
-class LoginState extends State<Login> {
+class _LoginState extends State<LoginForm> {
 
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build( BuildContext context ) {
-
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-            validator: (value) {
-              if( value == null || value.isEmpty) {
-                return "Bitte gib einen Text ein.";
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric( vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if( _formKey.currentState!.validate() ) {
-                  ScaffoldMessenger.of( context ).showSnackBar(
-                    const SnackBar( content: Text( "Eingabe wird verarbeitet") ),
-                  );
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                hintText: "Username",
+                hintStyle: TextStyle(color: Colors.white70, fontStyle: FontStyle.italic),
+              ),
+              validator: (value) {
+                if( value == null || value.isEmpty) {
+                  return "Please enter your username.";
                 }
+                return null;
               },
-              child: const Text("Login"),
             ),
-          ),
-        ],
-      ),
+
+            const SizedBox(height: 20),
+
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                hintText: "Password",
+                hintStyle: TextStyle(color: Colors.white70, fontStyle: FontStyle.italic),
+              ),
+              validator: (value) {
+                if( value == null || value.isEmpty) {
+                  return "Please enter your password.";
+                }
+                return null;
+              },
+            ),
+
+            const SizedBox(height: 40),
+
+            ElevatedButton(
+                onPressed: () {
+                  if( _formKey.currentState!.validate() ) {
+                    ScaffoldMessenger.of( context ).showSnackBar(
+                      const SnackBar( content: Text( "Eingabe wird verarbeitet") ),
+                    );
+                  }
+                },
+                child: const Text("Login"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),),
+              ),
+          ],
+        ),
+      )
     );
+  }
+}
+
+class Register extends StatefulWidget {
+
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
