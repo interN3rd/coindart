e3a09371303f1fbc4d6faa46f32369dcbc3cdbbc
SirOktarius <alexander.neumann@student.fh-kiel.de>
2021-10-16 16:16:32 +0200

    Firebase/Firestore implementation, added example user

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,82 @@
 import "package:flutter/material.dart";
 import "package:flutter_form_builder/flutter_form_builder.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(const Coindart());
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        /*if (snapshot.hasError) {
+          print("asd");
+          return SomethingWentWrong();
+        }*/
+
+        // Once complete, show your application
+        //if (snapshot.connectionState == ConnectionState.done) {
+          return const Coindart();
+        //}
+
+      },
+    );
+  }
+}
+
+class AddUser extends StatelessWidget {
+  final String name;
+
+  const AddUser(this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    // Create a CollectionReference called users that references the firestore collection
+    CollectionReference users = FirebaseFirestore.instance.collection('user');
+
+    Future<void> addUser() {
+      // Call the user's CollectionReference to add a new user
+      return users
+          .add({
+        'name': name
+      })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+
+    return TextButton(
+      onPressed: addUser,
+      child: Text(
+        "Add User",
+      ),
+    );
+  }
+
+}
 
 /// to prevent typos. Please use "Routes.<name of route>" to link different
 /// widgets/screens. And, of course, add any new route to this class before
@@ -97,6 +172,8 @@ class Coindart extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
 
                   children: <Widget> [
+
+                    const AddUser("Gunner"),
 
                     ElevatedButton(
 
