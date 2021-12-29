34e4f70347a39dad98cb4f1cc5e551eb536d262f
SirOktarius <alexander.neumann@student.fh-kiel.de>
2021-12-29 18:07:54 +0100

    IconButtons in Coinlist to add/remove Coins from Favorites (stored per user in Firestore Database). New Route "favorites" which only fetches and shows those coins from the API. Some visual bugs not yet fixed.

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:coindart/screens/register.dart';
 import 'package:coindart/screens/status.dart';
 import 'package:flutter/widgets.dart';
 import 'package:coindart/screens/coinlist.dart';
+import 'package:coindart/screens/favorites.dart';
 import 'package:coindart/screens/contact.dart';
 import 'package:coindart/screens/imprint.dart';
 
@@ -11,7 +12,8 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder> {
 
   "/login": ( BuildContext context ) => const Login(),
   "/register": ( BuildContext context ) => const Register(),
-  "/coinlist": ( BuildContext context ) => Coinlist(),
+  "/coinlist": ( BuildContext context ) => const Coinlist(),
+  "/favorites": ( BuildContext context ) => const Favorites(),
   "/contact": ( BuildContext context ) => Contact(),
   "/imprint": ( BuildContext context ) => const Imprint(),
   "/status": ( BuildContext context ) => Status(),
