367eeca6584cc7984e415655f39198f2a17ce94f
Pascal Groß <pascal.gross@student.fh-kiel.de>
2022-01-06 21:05:46 +0100

    made the price chart a litte more pretty

--- lib/components/price_chart.dart
+++ lib/components/price_chart.dart
@@ -1,42 +1,119 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class PriceChart extends StatelessWidget {
+class _LineChartGradientColor {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+}
+
+  class PriceChart extends StatelessWidget {
 
   const PriceChart( {Key? key} ) : super( key: key );
 
   @override
   Widget build( BuildContext context ) {
 
-    return SizedBox(
-      width: double.infinity,
-      height: 200,
-      child: LineChart( LineChartData(
-          titlesData: FlTitlesData(
-              show: true,
-              rightTitles: SideTitles(showTitles: false),
-              topTitles: SideTitles(showTitles: false)
+  return SizedBox(
+    width: double.infinity,
+    height: 200,
+    child: LineChart( LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );},
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+          },),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: SideTitles(showTitles: false),
+        topTitles: SideTitles(showTitles: false),
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          interval: 1,
+          getTextStyles: (context, value) => const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 2:
+                return 'NOV';
+                case 5:
+                  return 'DEC';
+                  case 8:
+                    return 'JAN';
+            }
+            return '';
+            },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          interval: 1,
+          getTextStyles: (context, value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
           ),
-          borderData: FlBorderData( show: false ),
-          lineBarsData: [
-            LineChartBarData(
-                spots: [
-                  const FlSpot( 0, 1 ),
-                  const FlSpot( 1, 3 ),
-                  const FlSpot( 2, 10 ),
-                  const FlSpot( 3, 7 ),
-                  const FlSpot( 4, 12 ),
-                  const FlSpot( 5, 13 ),
-                  const FlSpot( 6, 17 ),
-                  const FlSpot( 7, 15 ),
-                  const FlSpot( 8, 20 ),
-                ],
-                isCurved: true,
-                colors: [Colors.green]
-            )
-          ]
-      )),
-    );
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '10k';
+                case 3:
+                  return '30k';
+                  case 5:
+                    return '50k';
+            }
+            return '';
+            },
+          reservedSize: 32,
+          margin: 12,
+        ),
+      ),
+      borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 11,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: const [
+            FlSpot(0, 3),
+            FlSpot(2.6, 2),
+            FlSpot(4.9, 5),
+            FlSpot(6.8, 3.1),
+            FlSpot(8, 4),
+            FlSpot(9.5, 3),
+            FlSpot(11, 4),
+          ],
+          isCurved: true,
+          colors: _LineChartGradientColor().gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+            _LineChartGradientColor().gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
+    ),),
+  );
   }
-
 }
