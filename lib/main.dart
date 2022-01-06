c29313915bad14132337eefb8bf800c620c033a9
Pascal Groß <pascal.gross@student.fh-kiel.de>
2022-01-06 21:12:28 +0100

    enforced usage of portrait mode up

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:coindart/screens/coindart.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp( const Coindart() );
-
+  // CoinDart is to be used in mode 'portrait up' since we provide our users a couple of lists and charts that are best experienced in aforementioned mode
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then( (_) {
+        runApp(const Coindart() );
+      });
 }
